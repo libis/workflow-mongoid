ae2bc5c6d8c23628dc6e58e5df56f9243114541a
Kris Dekeyser <kris.dekeyser@libis.be>
2014-09-10 16:04:16 +0200

    update

--- lib/libis/workflow/mongoid/work_item.rb
+++ lib/libis/workflow/mongoid/work_item.rb
@@ -5,39 +5,27 @@ require 'libis/workflow/mongoid/base_model'
 module LIBIS
   module Workflow
     module Mongoid
-      class WorkItem < LIBIS::Workflow::WorkItem
+      class WorkItem
+        include LIBIS::Workflow::WorkItem
         include BaseModel
 
-        field :status, type: Symbol, default: :START
+        belongs_to :parent, class_name: 'LIBIS::Workflow::Mongoid::WorkItemModel', inverse_of: :items, dependent: :nullify
+        has_many :items, class_name: 'LIBIS::Workflow::Mongoid::WorkItemModel', inverse_of: :parent, autosave: true, dependent: :destroy
+
         field :options, type: Hash, default: -> { Hash.new }
         field :properties, type: Hash, default: -> { Hash.new }
+
         field :log_history, type: Array, default: -> { Array.new }
         field :status_log, type: Array, default: -> { Array.new }
+
         field :summary, type: Hash, default: -> { Hash.new }
 
-        has_many :items, class_name: 'LIBIS::Workflow::Mongoid::WorkItemModel', inverse_of: :parent, autosave: true, dependent: :destroy
-        belongs_to :parent, class_name: 'LIBIS::Workflow::Mongoid::WorkItemModel', inverse_of: :items, dependent: :nullify
-
-        index status: 1
-
         def name
-          self.to_string
+          self.options[:name]
         end
 
         def item_count
           self.items.length
-        end
-
-        def to_string
-          self.options[:name]
-        end
-
-        def <<(item)
-          return self unless item
-          self.items << item
-          self.save!
-          item.save!
-          self
         end
 
         def each
