54a0301d877cfa31c9a24b51e16472f695c184fc
Kris Dekeyser <kris.dekeyser@libis.be>
2015-06-10 18:29:38 +0200

    Reimplement Config on Tools::Config and version bump

--- lib/libis/workflow/mongoid/config.rb
+++ lib/libis/workflow/mongoid/config.rb
@@ -9,27 +9,12 @@ module Libis
   module Workflow
     module Mongoid
 
-     class Config
-       include Singleton
+     class Config < ::Libis::Workflow::Config
 
-       def database_connect(config_file = './mongoid.yml', environment = nil)
+       def self.database_connect(config_file = './mongoid.yml', environment = nil)
          ::Mongoid.load! config_file, environment
        end
 
-       def method_missing(name, *args, &block)
-         ::Libis::Workflow::Config.instance.send(name, *args, &block)
-       end
-
-       def self.const_missing(name)
-         return ::Libis::Workflow::Config.const_get(name) if ::Libis::Workflow::Config.const_defined?(name)
-         super(name)
-       end
-
-       private
-
-       def initialize
-         ::Libis::Workflow::Config.instance
-       end
       end
     end
   end
--- lib/libis/workflow/mongoid/version.rb
+++ lib/libis/workflow/mongoid/version.rb
@@ -3,7 +3,7 @@
 module Libis
   module Workflow
     module Mongoid
-      VERSION = '2.0.beta.5' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+      VERSION = '2.0.beta.6' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
     end
   end
 end
