0fb7291d82b3c8ed4bd0d631a4ca3bf2faafe903
Kris Dekeyser <kris.dekeyser@libis.be>
2015-12-07 19:36:16 +0100

    Worker bug fix: workflow -> job

--- lib/libis/workflow/mongoid/worker.rb
+++ lib/libis/workflow/mongoid/worker.rb
@@ -8,12 +8,12 @@ module Libis
 
       class Worker < Libis::Workflow::Worker
 
-        def get_workflow(workflow_config)
-          workflow_name = workflow_config[:name] if workflow_config.is_a? Hash
-          workflow_name ||= workflow_config.to_s
-          workflow = ::Libis::Workflow::Mongoid.find(name: workflow_name).first
-          raise RuntimeError.new "Workflow #{workflow_name} not found" unless workflow.is_a? ::Libis::Workflow::Mongoid::Workflow
-          workflow
+        def get_job(job_config)
+          job_name = job_config.delete(:name) if job_config.is_a? Hash
+          job_name ||= job_config.to_s
+          job = ::Libis::Workflow::Mongoid::Job.find(name: job_name).first
+          raise RuntimeError.new "Workflow #{job_name} not found" unless job.is_a? ::Libis::Workflow::Mongoid::Job
+          job
         end
 
       end
