dd0781e0348e29650df4070b89fec948e54caf7b
Kris Dekeyser <kris.dekeyser@libis.be>
2015-12-16 19:39:50 +0100

    Version bump: 2.0.beta.16

--- lib/libis/workflow/mongoid/version.rb
+++ lib/libis/workflow/mongoid/version.rb
@@ -3,7 +3,7 @@
 module Libis
   module Workflow
     module Mongoid
-      VERSION = '2.0.beta.15' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
+      VERSION = '2.0.beta.16' unless const_defined? :VERSION # the guard is against a redefinition warning that happens on Travis
     end
   end
 end
